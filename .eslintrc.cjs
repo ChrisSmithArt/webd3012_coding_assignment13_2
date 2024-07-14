f03bdf918c4b7da43a23ca17ba20b158e9304e0b
ChrisSmithArt <chrissmithart@proton.me>
2024-07-14 15:52:08 -0500

    added eslint and prettier into husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,7 @@
+// eslint-disable-next-line 
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
@@ -10,6 +11,9 @@ module.exports = {
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
+  globals: {
+    ethers: "readonly",
+  },
   rules: {
     "react-refresh/only-export-components": [
       "warn",
